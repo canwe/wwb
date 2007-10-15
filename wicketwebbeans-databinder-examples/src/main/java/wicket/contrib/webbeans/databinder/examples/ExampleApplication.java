cd1c3ab49445d16a4d5c7b08899603b84c0d3a70
mrsouthern <mrsouthern@1131db2a-1c84-11de-a027-6f5b8e30e268>
2007-10-15 12:45:23 +0000

    a little further
    git-svn-id: http://wicket-web-beans.googlecode.com/svn/branches/wicket-1.3-port@138 1131db2a-1c84-11de-a027-6f5b8e30e268

--- wicketwebbeans-databinder-examples/src/main/java/wicket/contrib/webbeans/databinder/examples/ExampleApplication.java
+++ wicketwebbeans-databinder-examples/src/main/java/wicket/contrib/webbeans/databinder/examples/ExampleApplication.java
@@ -5,6 +5,9 @@ import java.net.URL;
 import org.hibernate.cfg.AnnotationConfiguration;
 
 import net.databinder.DataApplication;
+import net.databinder.DataStaticService;
+
+import org.hibernate.Session;
 
 public class ExampleApplication extends DataApplication
 {
@@ -18,6 +21,24 @@ public class ExampleApplication extends DataApplication
 	public Class getHomePage()
 	{
 		return ListPage.class;
+	}
+	
+	@Override
+	protected void init()
+	{
+	    super.init();
+	    // load some example contacts
+	    Session session = DataStaticService.getHibernateSession();
+	    session.beginTransaction();
+	    Category category = new Category();
+	    category.setName("Friends");
+	    session.save(category);
+	    Contact contact = new Contact();
+	    contact.setName("John Smith");
+	    contact.setCategory(category);
+	    contact.setPhoneNumber("1-800-555-1234");
+	    session.save(contact);
+	    session.getTransaction().commit();
 	}
 	
 	@Override
--- wicketwebbeans-databinder-examples/src/main/java/wicket/contrib/webbeans/databinder/examples/ListPage.java
+++ wicketwebbeans-databinder-examples/src/main/java/wicket/contrib/webbeans/databinder/examples/ListPage.java
@@ -1,5 +1,6 @@
 package wicket.contrib.webbeans.databinder.examples;
 
+import org.apache.wicket.Page;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.ajax.AjaxRequestTarget;
@@ -15,24 +16,22 @@ public class ListPage extends WebPage
     {
         Form form = new Form("form");
         add(form);
-        form.add(panel = new ContactBeanListPanel("beanTable", Contact.class));
+        form.add(panel = new ContactBeanListPanel("beanTable", Contact.class, this));
     }
     
-    private static class ContactBeanListPanel extends DataBeanListPanel
+    static class ContactBeanListPanel extends DataBeanListPanel
     {
-        public ContactBeanListPanel(String id, Class beanClass)
+        private Page returnPage;
+        
+        public ContactBeanListPanel(String id, Class beanClass, Page returnPage)
         {
             super(id,beanClass);
-        }
-        
-        public void create(AjaxRequestTarget target, Form form, Object bean)
-        {
-            
+            this.returnPage = returnPage;
         }
         
         public void edit(AjaxRequestTarget target, Form form, Object bean)
         {
-            
+            setResponsePage(new EditPage(bean,returnPage));
         }
     }
 }
