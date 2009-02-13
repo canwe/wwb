d57abbdaad6c32e07efc3592364795f80bb898d7
dtoffe <dtoffe@1131db2a-1c84-11de-a027-6f5b8e30e268>
2009-02-13 06:04:11 +0000

    Revert patch for [BUG: 2526425 ] beanForm.setFocusField not working
    git-svn-id: http://wicket-web-beans.googlecode.com/svn/trunk@322 1131db2a-1c84-11de-a027-6f5b8e30e268

--- wicketwebbeans/src/main/java/net/sourceforge/wicketwebbeans/fields/AbstractField.java
+++ wicketwebbeans/src/main/java/net/sourceforge/wicketwebbeans/fields/AbstractField.java
@@ -23,10 +23,7 @@ import net.sourceforge.wicketwebbeans.containers.BeanForm;
 import net.sourceforge.wicketwebbeans.model.BeanPropertyModel;
 import net.sourceforge.wicketwebbeans.model.ElementMetaData;
 
-import org.apache.wicket.Component;
-import org.apache.wicket.behavior.AbstractBehavior;
 import org.apache.wicket.behavior.SimpleAttributeModifier;
-import org.apache.wicket.markup.ComponentTag;
 import org.apache.wicket.markup.html.form.FormComponent;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.IModel;
@@ -112,14 +109,6 @@ abstract public class AbstractField extends Panel implements Field
         if (defaultValue != null && Strings.isEmpty(getModelObjectAsString())) {
             field.setModelValue(defaultValue);
         }
-
-        field.add( new AbstractBehavior() {
-            public void onComponentTag(Component component, ComponentTag tag)
-            {
-                tag.put("id", elementMetaData.getPropertyName());
-                super.onComponentTag(component, tag);
-            }
-        });
     }
     
     /**
